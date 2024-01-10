669efc3ad7f81ad451f49a290f2b3bbcd3492d9e
Laura <ldelcano@gmail.com>
2024-01-10 15:56:28 +0100

    Working version with sleep function to keep it alive - to review

--- scipion3cudagl/docker-entrypoint.sh
+++ scipion3cudagl/docker-entrypoint.sh
@@ -15,20 +15,9 @@ echo -e "$USER_PASS\n$USER_PASS" | passwd $S_USER
 service ssh start
 
 # Install cryosparc
-#if ! [ -z $CRYOSPARC_LICENSE ] && [ -z $NOGPU ];
-#then su - ${S_USER} -c "${S_USER_HOME}/install_cryosparc.sh $CRYOSPARC_LICENSE";
-#else echo "No cryosparc because no gpu in the server"; fi
-
-# Update cryosparc hostname and license
-if ! [ -z $CRYOSPARC_LICENSE ] && [ -z $NOGPU ]; then
-    sed -i -e "s+CRYOSPARC_MASTER_HOSTNAME=.*+CRYOSPARC_MASTER_HOSTNAME=\"$HOSTNAME\"+g" $S_USER_HOME/cryosparc_${CRYOSPARC_VERSION}/cryosparc_master/config.sh
-    sed -i -e "s+CRYOSPARC_LICENSE_ID=.*+CRYOSPARC_LICENSE_ID=\"$CRYOSPARC_LICENSE\"+g" $S_USER_HOME/cryosparc_${CRYOSPARC_VERSION}/cryosparc_master/config.sh
-    sudo -u $S_USER $S_USER_HOME/cryosparc_${CRYOSPARC_VERSION}/cryosparc_master/bin/cryosparcm restart
-
-    set +e
-    # Connect worker
-    sudo -u $S_USER $S_USER_HOME/cryosparc_${CRYOSPARC_VERSION}/cryosparc_worker/bin/cryosparcw connect --worker $HOSTNAME --master $HOSTNAME --nossd
-fi
+if ! [ -z $CRYOSPARC_LICENSE ];
+then su - ${S_USER} -c "${S_USER_HOME}/install_cryosparc.sh $CRYOSPARC_LICENSE";
+else echo "No cryosparc because no gpu in the server"; fi
 
 echo $USE_DISPLAY
 export WEBPORT=590${USE_DISPLAY}
@@ -42,5 +31,4 @@ echo $MYVNCPASSWORD
 su - ${S_USER} -c "echo $MYVNCPASSWORD | /opt/TurboVNC/bin/vncpasswd -f > $S_USER_HOME/.vnc/passwd"
 chmod 0600 $S_USER_HOME/.vnc/passwd
 su - ${S_USER} -c "/opt/TurboVNC/bin/vncserver -novnc /usr/share/novnc -xstartup /tmp/xsession -x509cert /opt/TurboVNC/self.crt -x509key /opt/TurboVNC/self.key"
-sleep 1000
-#websockify ${WEBPORT} --web=/usr/share/novnc --wrap-mode=ignore -- /opt/TurboVNC/bin/vncserver ${DISPLAY} -xstartup /tmp/xsession
+sleep 10000
--- scipion3cudagl/install_cryosparc.sh
+++ scipion3cudagl/install_cryosparc.sh
@@ -7,8 +7,8 @@ S_USER_HOME=/home/$S_USER
 
 export USER=$S_USER
 
-CRYOSPARC_VERSION="$1"
-CRYOSPARC_LICENSE="$2"
+CRYOSPARC_VERSION="4.4.0"
+CRYOSPARC_LICENSE="$1"
 
 mkdir ${S_USER_HOME}/cryosparc_${CRYOSPARC_VERSION}
 curl -L https://get.cryosparc.com/download/master-v${CRYOSPARC_VERSION}/${CRYOSPARC_LICENSE} -o ${S_USER_HOME}/cryosparc_${CRYOSPARC_VERSION}/cryosparc_master.tar.gz
@@ -23,3 +23,7 @@ echo -e "CRYOSPARC_HOME = $S_USER_HOME/cryosparc_${CRYOSPARC_VERSION}" >> $S_USE
 echo -e "CRYOSPARC_USER = i2pc@cnb.csic.es" >> $S_USER_HOME/scipion3/config/scipion.conf
 
 ln -s ${S_USER_HOME}/ScipionUserData/scipion_projects ${S_USER_HOME}/cryosparc_${CRYOSPARC_VERSION}/scipion_projects
+
+# Install cryosparc plugin
+${S_USER_HOME}/scipion3/scipion3 python -m pip install pyem
+${S_USER_HOME}/scipion3/scipion3 installp -p scipion-em-cryosparc2
